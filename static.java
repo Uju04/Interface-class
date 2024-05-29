c2643adf3794b74beffa359a6b5ab7c1766ed01b
Uju04 <129797892+Uju04@users.noreply.github.com>
2024-05-29 11:05:53 +0100

    Add files via upload

---
+++ static.java
@@ -0,0 +1,8 @@
+public class static{
+    static void addNumber(int a, int b){
+        System.out.println(a+b);
+        public static void main(string[]args){
+            addNumber(a:6,b:8);
+        }
+    }
+}
